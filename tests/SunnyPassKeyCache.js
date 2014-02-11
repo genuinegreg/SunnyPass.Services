d3d3b82e37fd405b6810e9ed634df17bd20599b2
Grégoire Audoux <projects@gregoire-audoux.fr>
2014-02-11 14:01:10 +0100

    Change KeyCache api. No more promises.  CacheKey.get() now return undefined or a value depending if there is an entry for the key

--- tests/SunnyPassKeyCache.js
+++ tests/SunnyPassKeyCache.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Service: SunnyPassKeyCache', function() {
+describe('Service: SunnyPassKeyCache', function () {
 
     // load the service's module
     beforeEach(module('SunnyPass.Services'));
@@ -12,126 +12,29 @@ describe('Service: SunnyPassKeyCache', function() {
         KeyCache.invalidate();
     }));
 
-    describe('.put()', function() {
-        it('should add key to the cache', inject(function($rootScope, Crypto) {
+    describe('.put()/.get()', function () {
+        it('should add key to the cache', inject(function ($rootScope, Crypto) {
 
             var key = 'key_' + Crypto.generateKey(32);
             var expectedValue = 'expectedKey_' + Crypto.generateKey(32);
 
-            var resolvedValue;
+            // puts key in cache
+            KeyCache.put(key, expectedValue);
 
-            runs(function() {
+            var resolvedValue = KeyCache.get(key);
 
-                KeyCache.put(key, expectedValue);
-
-                var promise = KeyCache.get(key);
-
-                promise.then(
-                    function resolved(resultValue) {
-                        resolvedValue = resultValue;
-                    });
-
-                $rootScope.$apply();
-            })
-
-            waitsFor(function() {
-                return resolvedValue;
-            }, 'value to be defined', 500);
-
-            runs(function() {
-                expect(resolvedValue).toEqual(expectedValue);
-            })
+            expect(resolvedValue).toEqual(expectedValue);
         }))
 
-        it('should notify missing key and resolve it on KeyCache.put() call', inject(function($rootScope, Crypto, $timeout) {
+        it('should return undefined if their is not entry in cache', inject(function ($rootScope, Crypto, $timeout) {
             var key = 'key_' + Crypto.generateKey(32);
-            var expectedValue = 'expectedKey_' + Crypto.generateKey(32);
 
-            var resolvedValue;
-            var hasBeenNotified;
+            var resolvedValue = KeyCache.get(key);
+            expect(resolvedValue).not.toBeDefined();
 
-            runs(function() {
-
-                KeyCache.get(key).then(
-                    function resolved(resultValue) {
-                        console.log('resolved !!!', resultValue)
-                        resolvedValue = resultValue;
-                    },
-                    undefined,
-                    function notified() {
-                        console.log('notified !!!');
-                        hasBeenNotified = true;
-                    });
-
-
-                console.log('KeyCache.het();')
-                $rootScope.$apply();
-
-                $timeout(function() {
-                    $timeout.flush();
-                    KeyCache.put(key, expectedValue);
-                    $timeout.flush();
-                    console.log('KeyCache.put()')
-                }, 500);
-
-                $timeout.flush();
-                $rootScope.$apply();
-            })
-
-            waitsFor(function() {
-                console.log(resolvedValue, hasBeenNotified)
-                return resolvedValue && hasBeenNotified;
-            }, 'resolvedValue and hasBeenNotified to be defined', 1000);
-
-            runs(function() {
-                expect(resolvedValue).toEqual(expectedValue);
-            })
         }));
 
 
-        it('should queue multiple missing key and resolve them on KeyCache.put() call', inject(function($rootScope, Crypto, $timeout) {
-            var key = 'key_' + Crypto.generateKey(32);
-            var expectedValue = 'expectedKey_' + Crypto.generateKey(32);
-
-            var resolvedValue1;
-            var resolvedValue2;
-
-            runs(function() {
-
-                KeyCache.get(key).then(
-                    function resolved(resultValue) {
-                        resolvedValue1 = resultValue;
-                    });
-
-                $rootScope.$apply();
-
-                KeyCache.get(key).then(
-                    function resolved(resultValue2) {
-                        resolvedValue2 = resultValue2;
-                    });
-
-                $rootScope.$apply();
-
-                $timeout(function() {
-                    $timeout.flush();
-                    KeyCache.put(key, expectedValue);
-                    $timeout.flush();
-                    console.log('KeyCache.put()')
-                }, 500);
-
-                $timeout.flush();
-                $rootScope.$apply();
-            })
-
-            waitsFor(function() {
-                return resolvedValue1 && resolvedValue2;
-            }, 'resolvedValue and hasBeenNotified to be defined', 1000);
-
-            runs(function() {
-                expect(resolvedValue1).toEqual(expectedValue);
-                expect(resolvedValue2).toEqual(expectedValue);
-            })
-        }));
     })
 
 
